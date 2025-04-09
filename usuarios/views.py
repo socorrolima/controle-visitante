68f0d0abe10d172f92cd39f13421a94e06f59aa5
Socorro Lima <socorrolima41@gmail.com>
2025-04-09 15:07:55 -0300

    registrar visitante

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from visitantes.models import Visitante
 
 def index(request):
+   todos_visitantes = Visitante.objects.all()
    context = {
       "nome_pagina": "Inicio da dashboard",
+      "todos_visitantes":todos_visitantes,
    }
    return render(request, 'index.html', context)
 
